d52ecdfd8b17d52d5247a4208623e19cf649d1a5
harpreet <harpreet.singh@splashmath.com>
2018-07-19 18:54:41 +0530

    commented

--- lib/activeuuid/uuid.rb
+++ lib/activeuuid/uuid.rb
@@ -103,7 +103,7 @@ module ActiveUUID
       class_attribute :_uuid_generator, instance_writer: false
       self._uuid_generator = :random
 
-      singleton_class.instantiate
+      # singleton_class.alias_method_chain :instantiate, :uuid
       before_create :generate_uuids_if_needed
     end
 
